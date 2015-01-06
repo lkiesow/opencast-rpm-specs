6931f1d05328fcf36f5187b23e2e37d069691eac
Michal Fiala <fiala@mfiala.net>
2015-01-06 18:26:19 +0100

    Add rpmspec path. Delete building variables (make, gcc). Add translation array (install ITEM1 means install ITEM2). Add logging. Rewrite help(). Spec install rpms also via names from translation array. Skip installed packages faster. Rewrite input arguments.

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
+# 
+# Build rpms for specific spec file. Script follows spec file dependencies and
+# install it first via other spec file, second via yum.  
+# 
 
-# Idea
-#
 # install()
 # {
 #       spec file exists? {
 #               for all needed packages in spec file call install(package)
-#               returns if package is installed localy from spec
+#               returns if package is installed localy from spec file
 #               compile, build, install rpm 
 #       } else {
 #               if package is not installed, then install it via yum
@@ -16,29 +18,38 @@
 # paths
 specdir=$HOME/matterhorn-rpms/specs/
 sourcedir=~/rpmbuild/SOURCES/
-# default /usr/local is prefered
+# path to rpmspec tool (package rpm contains rpmspec) 
+rpmspec=/usr/local/bin/rpmspec
+
+# Set PATH, prefere default system paths
+# default /usr/local is prefered, brake system rpm
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin:$PATH"
+
+# install ITEM1 means install ITEM2
+eqarr="
+librtmp-devel rtmpdump
+cairo-gobject-devel cairo
+"
 
 # debug, be more verbose
 debug="1"
 
-# compilation flags
-# compile in N threads
-export MAKEOPTS="-j5"
-#export CFLAGS="-O2 -pipe"
-#export CXXFLAGS="${CFLAGS}"
-#export CHOST="x86_64-pc-linux-gnu"
+# log 
+log="1"
+log_file=~/build.log
 
 # reporting message
 function message()
 {
     echo "$(date +%F-%T) M: $@"
+    [ "$log" = "1" ] && echo "$(date +%F-%T) M: $@" >> $log_file
 }
 
 # error, exit script
 function error()
 {
     echo "$(date +%F-%T) E: $@"
+    [ "$log" = "1" ] && echo "$(date +%F-%T) E: $@" >> $log_file
     exit 1
 }
 
@@ -46,15 +57,33 @@ function error()
 function xeval()
 {
     cmd=$@
-    [ "$debug" == "1" ] && echo "$(date +%F-%T) D: $cmd"
+    [ "$debug" = "1" ] && echo "$(date +%F-%T) C: $cmd"
+    [ "$log" = "1" ] && echo "$(date +%F-%T) C: $cmd" >> $log_file
     eval $cmd || error "$cmd"
+}
+
+# help
+function help()
+{
+cat <<EOF
+$0 -h | -i | -p <PACKAGE>
+    
+    -h ... help
+    -i ... init system for rpm building
+    -p <PACKAGE> ... make rpms for <PACKAGE> package
+
+    $0 -i
+    $0 -p ffmpeg
+EOF
+    exit 0
 }
 
 # install spec file
 function install_specs()
 {
     rpm="$1"
-    specfile="$2"
+    basetarget="$2"
+    specfile="$3"
 
     message "Install spec $specfile, rpm $rpm"
 
@@ -63,10 +92,10 @@ function install_specs()
 
     # application was installed locally? (via rpm from spec file)
     if echo "$line" | egrep "^$rpm.*(@/$rpm|installed)" >& /dev/null; then
-	echo "Package $rpm installed from spec, skip package"
+	message "Package $rpm installed from spec, skip package"
 	return 0
     elif [ -n "$line" ]; then
-	echo "package $prm installed via online repo, remove and install via specs"
+	message "package $prm installed via online repo, remove and install via specs"
 	# remove package without dependencies
 	#sudo yum remove -y $rpm
 	xeval "sudo rpm -e $rpm --nodeps"
@@ -84,10 +113,18 @@ function install_specs()
     xeval "rpmbuild -ba $specfile"
 
     # Install package
-    xeval "sudo yum localinstall -y ~/rpmbuild/RPMS/x86_64/*${rpm}*"
+    # basetarget
+    local toinst="~/rpmbuild/RPMS/x86_64/*${basetarget}*"
+    # based on eqarr
+    local tmp="$(echo "$eqarr" | grep " $basetarget" | cut -d' ' -f1)"
+    [ -n "$tmp" ] && toinst="$toinst ~/rpmbuild/RPMS/x86_64/*${tmp}*"
+    # based on eqarr minus suffix devel
+    tmp="${tmp%%-devel}"
+    [ -n "$tmp" ] && toinst="$toinst ~/rpmbuild/RPMS/x86_64/*${tmp}*"
+    xeval "sudo yum localinstall -y $toinst"
 }
 
-# Install required packages pro running 
+# Install required packages for running 
 function init()
 {
     # Install basic packages
@@ -109,62 +146,65 @@ function init()
     # Copy patches to working directory
     cd "$sourcedir"
     xeval "cp $HOME/matterhorn-rpms/patch/*.patch ./"
-    
-    # make tmdir for spectool from localy compiled rpm
-    [ ! -d "/usr/local/var/tmp/" ] && 
-        xeval "sudo mkdir -p /usr/local/var/tmp/"
-    xeval "chmod 1777 /usr/local/var/tmp/"
-}
-
-# help
-function help()
-{
-    echo "$0 init | package"
-    echo "$0 init"
-    echo "$0 ffmpeg"
-    exit 0
 }
 
 # main function, the base of recursion
 function main() 
 {
     local target="$1"
+    local tmptar=""
 
     message "Start with target $target"
 
-    # spec file exists?
+    # lookup eqarr 
+    tmptar="$(echo "$eqarr" | grep "^$target " | cut -d' ' -f2)"
+    if [ -n "$tmptar" ]; then
+       message "Target found in eqarr => $target = $tmptar"
+       target="$tmptar"
+    fi
+
     # remove -devel suffix (devel version goes from its original package)
+    local basetarget="${target%%-devel}"
     # add suffix .spec
-    local specfile="$specdir/${target%%-devel}.spec"
+    local specfile="$specdir/$basetarget.spec"
+
+    # spec file exists?
     if [ -f "$specfile" ]; then
         message "Specfile $specfile exists, following dependencies"
         # Solve dependencies via rekursion calls
-        breqs="$(/usr/local/bin/rpmspec -q --buildrequires $specfile)"
+        breqs="$($rpmspec -q --buildrequires $specfile)"
         rc=$?
         [ "$rc" != "0" ] && error "rpmspec -q --buildrequires returns $rc"
         for breq in $(echo "$breqs" | cut -d' ' -f1); do
-            message "Run main $breq"
+            message "Run main $breq (dependence of $(basename $specfile))"
             main $breq
         done
-        install_specs $target $specfile 
-    else
+        install_specs $target $basetarget $specfile 
+    elif ! yum list installed $target >& /dev/null; then
         message "Install target $target via yum"
         xeval "sudo yum install -y -q $target"
+    else
+        message "Target $target is installed, skipped"
     fi
 }
 
-# get user target 
-target="$1"
+# parse input arguments
+while [ "$#" != "0" ]; do
+    case "$1" in
+        -i) init
+            exit 0
+            ;;
+        -p) shift
+            target="$1"
+            ;;
+        *)  help
+            ;;
+    esac
+    shift
+done
 
 # target is required
 [ -z "$target" ] && help
 
-# init
-if [ "$target" = "init" ]; then
-    init
-    exit 0
-fi
-
-# start interation no 1
+# start iteration #1
 main $target
-
